f7c4ad488405369f0754a42ad565e3785066e7e5
Dina <sipos.edina02@gmail.com>
2022-01-05 18:31:39 +0100

    done

--- src/lesson-01-basic-types.ts
+++ src/lesson-01-basic-types.ts
@@ -4,24 +4,24 @@ export default class calculator {
 
     // Hozd létre a következő változókat a megfelelő típusannotációval.
     /** @type {number} members - value: 11 */
-    
+    members: number = 11;
 
     /** @type {number} baseColor - value: a3f4d5 (hexadecimal number) */
-    
+    baseColor: number = 10745045;
 
     /** @type {number} role - value: 1001 (binary number) */
-    
+    role: number = 9;
 
     /** @type {number} systemID - value: 7700 (octal number) */
-    
+    systemID: number = 4032;
 
     /** @type {number} average - value: 4.5 */
-    
+    average: number = 4.5;
 
     /** @type {string} adminName - value: szabadon választott szöveg */
-    
+    adminName: string = "Pipacs";
 
     /** @type {string} sumPoints - value: 'Members have <this.members és a this.average szorzata> points' (template string) */
-
+    sumPoints: string =  `Members have ${this.members * this.average} points`;
 
 }
